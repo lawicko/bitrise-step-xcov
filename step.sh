81070145146f62c5a5419e890728e1672a7cba31
Jakub Lawicki <lawicko@gmail.com>
2020-04-30 22:04:02 +0200

    Added scheme support for the projects with multiple schemes

--- step.sh
+++ step.sh
@@ -3,6 +3,7 @@ set -ex
 
 echo "xcresult_file_path: ${xcresult_file_path}"
 echo "report_path: ${report_path}"
+echo "scheme: ${scheme}"
 
 echo 'Checking xcov availability...'
 if ! [ -x "$(command -v xcov)" ]; then
@@ -11,7 +12,12 @@ if ! [ -x "$(command -v xcov)" ]; then
 fi
 echo "xcov installed in $(command -v xcov), generating report..."
 
-xcov -f ${xcresult_file_path} -o ${report_path}
+FLAGS=''
+if [ -s "${scheme}" ] ; then
+  FLAGS=$FLAGS' -s '"${scheme}"
+fi
+
+xcov -f ${xcresult_file_path} -o ${report_path} ${FLAGS}
 
 filename="${report_path}/index.html"
 envman add --key XCOV_REPORT_PATH --value $filename
