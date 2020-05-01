cbde5e4e27408931bd3821528e9c26395799c529
Jakub Lawicki <lawicko@gmail.com>
2020-05-01 12:40:31 +0200

    Replaced the -s check with the default for the scheme

--- step.sh
+++ step.sh
@@ -12,14 +12,20 @@ if ! [ -x "$(command -v xcov)" ]; then
 fi
 echo "xcov installed in $(command -v xcov), generating report..."
 
-FLAGS=''
-if [ -s "${scheme}" ] ; then
-  FLAGS=$FLAGS' -s '"${scheme}"
+COMMAND="xcov -f ${xcresult_file_path} -o ${report_path}"
+#echo "initial command: $COMMAND"
+FLAGS=""
+if [ "$scheme" ]; then
+  FLAGS="-s $scheme $FLAGS"
+#  echo "setting flags to: $FLAGS"
 fi
+#echo "flags to apply: $FLAGS"
+COMMAND="$COMMAND $FLAGS"
+#echo "final command: $COMMAND"
 
-xcov -f ${xcresult_file_path} -o ${report_path} ${FLAGS}
+$COMMAND
 
 filename="${report_path}/index.html"
-envman add --key XCOV_REPORT_PATH --value $filename
+envman add --key XCOV_REPORT_PATH --value "$filename"
 
 exit 0
